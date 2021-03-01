bc8d46fd064b35041d84bb2dbb14d955f4a590d1
Whitney H <whitney.holmes@unb.ca>
2021-02-28 22:11:33 -0400

    Cleaned up isDuplicate

--- src/isDuplicate.c
+++ src/isDuplicate.c
@@ -4,19 +4,21 @@
 #include "functions.h"
 
 /**
- * Compares the latest addition to an array of strings to
- * check for duplicates.
+ * Compares an array of strings to itself to check for duplicates.
  * @author Whitney Holmes 3502092
  * @returns 0 if duplicate
  * @returns 1 if not duplicate
  */
-int isDuplicate(char * charArray[], char * tagArray[], int arrayLength) {
-    char * tag = charArray[0]; //Tag to check
-    //Loop through the array
-    for(int index = 0; index < arrayLength -1; index++) {
-        if(strcmp(tagArray[index], tag) == 0) {
-             return 0; //If duplicate, return true
-        }
+int isDuplicate(char * tagArray[], int arrayLength) {
+    int i;
+    int j;
+
+    for(i = 0; i < arrayLength; i++) {
+        for(j = i + 1; j < arrayLength; j++) {
+            if(strcmp(tagArray[i], tagArray[j]) == 0) {
+                return 0; //If duplicate, return true
+            }   
+        }  
     }
-    return 1; //Return false
+    return 1; //Return false if distinct
 }
